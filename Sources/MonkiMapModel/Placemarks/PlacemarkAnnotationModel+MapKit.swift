e3274cb122bb7dc648c5d14899b363d0353cfa77
Rémi Bardon <remi.bardon.dev@gmail.com>
2020-10-16 10:26:38 +0200

    Add MapKit helper computed var to PlacemarkAnnotationModel

---
+++ Sources/MonkiMapModel/Placemarks/PlacemarkAnnotationModel+MapKit.swift
@@ -0,0 +1,24 @@
+//
+//  PlacemarkAnnotationModel+MapKit.swift
+//  MonkiMapModel
+//
+//  Created by Rémi Bardon on 16/10/2020.
+//  Copyright © 2020 Monki Projects. All rights reserved.
+//
+
+#if canImport(MapKit)
+import MapKit
+
+public extension PlacemarkAnnotationModel {
+	
+	var mkAnnotation: MKAnnotation {
+		let annotation = MKPointAnnotation()
+		annotation.coordinate = CLLocationCoordinate2D(
+			latitude: latitude,
+			longitude: longitude
+		)
+		return annotation
+	}
+	
+}
+#endif
