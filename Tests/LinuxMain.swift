9d95ebf702f38a0b85be6b17bd0a6116ff3c98aa
RemiBardon <remi.bardon.dev@gmail.com>
2020-10-26 14:50:11 +0000

    Deploying to gh-pages from  @ 54dcb970b26c3e63cb724bf6aca5f68046f61155 🚀

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-import MonkiMapModelTests
-
-var tests = [XCTestCaseEntry]()
-tests += PlacemarkBenefitTests.allTests()
-tests += PlacemarkBenefitLocalizedTests.allTests()
-XCTMain(tests)
