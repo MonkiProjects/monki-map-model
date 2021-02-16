068fad74810c646c7d1974229fd3d84279c21cd2
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-02-16 20:55:34 +0100

    Add public initializer

--- Sources/MonkiMapModel/Placemarks/Details/PlacemarkDetails+Public.swift
+++ Sources/MonkiMapModel/Placemarks/Details/PlacemarkDetails+Public.swift
@@ -13,13 +13,36 @@ public extension Placemark.Details {
 	
 	struct Public: Codable {
 		
+		public typealias Properties = [Placemark.Property.Localized]
+		
 		public let caption: String
 		public let satelliteImage: URL
 		public let images: [URL]
-		public let location: Placemark.Location.Public?
-		public let features, goodForTraining, benefits, hazards: [Placemark.Property.Localized]
+		public let location: Placemark.Location.Public
+		public let features, goodForTraining, benefits, hazards: Properties
 		
 		public var allImages: [URL] { images + [satelliteImage] }
+		
+		public init(
+			caption: String,
+			satelliteImage: URL,
+			images: [URL] = [],
+			location: Placemark.Location.Public,
+			features: Properties = [],
+			goodForTraining: Properties = [],
+			benefits: Properties = [],
+			hazards: Properties = []
+		) {
+			self.caption = caption
+			self.satelliteImage = satelliteImage
+			self.images = images
+			self.location = location
+			self.features = features
+			self.goodForTraining = goodForTraining
+			self.benefits = benefits
+			self.hazards = hazards
+		}
+		
 	}
 	
 }
