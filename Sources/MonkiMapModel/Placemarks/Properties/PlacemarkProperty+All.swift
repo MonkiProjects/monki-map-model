f996be2facc7cf596deae7a7bccd9b29677409ff
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-02-17 20:41:10 +0100

    Add `Placemark.Property.all`

---
+++ Sources/MonkiMapModel/Placemarks/Properties/PlacemarkProperty+All.swift
@@ -0,0 +1,24 @@
+//
+//  PlacemarkProperty+All.swift
+//  MonkiMapModel
+//
+//  Created by Rémi Bardon on 17/02/2021.
+//  Copyright © 2021 Monki Projects. All rights reserved.
+//
+
+import Foundation
+
+extension Placemark.Property {
+	
+	public static func all(in locale: Locale = Locale.current) -> [Self] {
+		return Internal.all(in: locale)
+			.map { Self.init(id: $0.id, kind: $0.kind) }
+	}
+	
+	public static func all(kind: Kind, in locale: Locale = Locale.current) -> [Self] {
+		return Internal.all(in: locale)
+			.filter { $0.kind == kind }
+			.map { Self.init(id: $0.id, kind: $0.kind) }
+	}
+	
+}
